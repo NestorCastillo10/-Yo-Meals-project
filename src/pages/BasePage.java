29731c35da0cc9982f29dd2b3fca1319426e2fd7
Ivica Nikolic <ivicanklc@gmail.com>
2020-09-28 21:54:42 +0200

    BasePage class is created

--- src/pages/BasePage.java
+++ src/pages/BasePage.java
@@ -1,5 +1,21 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 public abstract class BasePage {
 
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+
+	public BasePage(WebDriver driver, WebDriverWait wait) {
+		super();
+		this.driver = driver;
+		this.wait = wait;
+	}
+
+	
+	
+	
+	
 }
