0d65c858b195659a4f2f2a5c5018835cf823cd01
Ivica Nikolic <ivicanklc@gmail.com>
2020-09-29 21:16:07 +0200

    MealPage class created

---
+++ src/pages/MealPage.java
@@ -0,0 +1,54 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class MealPage extends BasePage {
+
+	// ctor
+	public MealPage(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
+
+	public void addMeal(String qty) {
+		this.clickOnMealImg();
+		this.wait.until(ExpectedConditions.elementToBeClickable(getAddToCart()));
+
+		this.clickOnQty();
+		this.getQty().clear();
+		this.getQty().sendKeys(qty);
+		this.getAddToCart();
+	}
+
+	public void addToFavotife() {
+		this.getFavBtn().click();
+	}
+
+	public void clickOnMealImg() {
+		this.getMealImage().click();
+	}
+
+	public void clickOnQty() {
+		this.getQty().click();
+	}
+
+	// getters
+	public WebElement getFavBtn() {
+		return this.driver.findElement(By.id("item_118"));
+	}
+
+	public WebElement getMealImage() {
+		return this.driver.findElement(By.xpath("(//img[@alt='Product Image'])[3]"));
+	}
+
+	public WebElement getAddToCart() {
+		return this.driver.findElement(By.xpath("//a[contains(.,'Add To Cart')]"));
+	}
+
+	public WebElement getQty() {
+		return this.driver.findElement(By.name("product_qty"));
+	}
+}
