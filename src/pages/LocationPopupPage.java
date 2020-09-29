1956187fb3d45ab3f86cad3b7b153b6cf87c3cd0
Ivica Nikolic <ivicanklc@gmail.com>
2020-09-29 12:36:47 +0200

    LocationPopupPage class created

---
+++ src/pages/LocationPopupPage.java
@@ -0,0 +1,65 @@
+package pages;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class LocationPopupPage extends BasePage {
+
+	// ctor
+	public LocationPopupPage(WebDriver driver, WebDriverWait wait) {
+		super(driver, wait);
+	}
+
+	public void showPopup() {
+		this.getSelectLocationInHeader().click();
+	}
+
+	public void setLocation(String locationName) {
+
+		this.clickOnKeyword();
+		String dataValue = this.getDataValue(locationName);
+
+		JavascriptExecutor jse = (JavascriptExecutor) this.driver;
+		String script = "arguments[0].value=arguments[1]";
+		jse.executeScript(script, locationName, dataValue);
+
+	}
+
+	public String getDataValue(String locationName) {
+		return this.getLocationItem(locationName).getAttribute("data-value");
+
+	}
+
+	public void clickOnKeyword() {
+		this.getKeyword().click();
+	}
+
+	// getters
+	public WebElement getSelectLocationInHeader() {
+		return this.driver.findElement(By.xpath("//div[@class='location-selector']"));
+	}
+
+	public WebElement getCloseBtn() {
+		return this.driver.findElement(By.xpath("//a[@class='close-btn close-btn-white']"));
+	}
+
+	public WebElement getKeyword() {
+		return this.driver.findElement(By.id("locality_keyword"));
+	}
+
+	public WebElement getLocationItem(String locationName) {
+		return this.driver.findElement(By.xpath("//li/a[contains(text(), 'Test - Caxito')]/parent::*"));
+	}
+
+	public WebElement getLocationInput() {
+		return this.driver.findElement(By.id("location_id"));
+	}
+
+	public WebElement getSubmit() {
+		return this.driver.findElement(By.name("btn_submit"));
+	}
+
+}
