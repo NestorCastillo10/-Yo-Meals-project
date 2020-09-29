76ff49de7bcdd4ac2c60a594ed0f16a4300ca64d
Ivica Nikolic <ivicanklc@gmail.com>
2020-09-29 11:18:04 +0200

    BaseTest class created

--- src/test/BaseTest.java
+++ src/test/BaseTest.java
@@ -1,5 +1,57 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.io.FileHandler;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.ITestResult;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.asserts.SoftAssert;
+
 public abstract class BaseTest {
 
+	protected WebDriver driver;
+	protected WebDriverWait wait;
+	protected String baseUrl = "demo.yo-meals.com";
+	protected SoftAssert softAssert;
+	protected String email = "customer@dummyid.com";
+	protected String password = "12345678a";
+
+	@BeforeClass
+	public void setUp() {
+		System.setProperty("webdriver.chrome.driver", "driver-lib\\chromedriver.exe");
+		this.driver = new ChromeDriver();
+		this.wait = new WebDriverWait(driver, 15);
+		this.softAssert = new SoftAssert();
+
+		this.driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
+		this.driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+		this.driver.manage().window().maximize();
+	}
+
+	@AfterMethod
+	public void afterMethod(ITestResult result) throws IOException {
+		if (result.getStatus() == ITestResult.FAILURE) {
+			File ss = ((TakesScreenshot) this.driver).getScreenshotAs(OutputType.FILE);
+			String fileName = new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss'.png'").format(new Date());
+			FileHandler.copy(ss, new File("screenshots/" + result.getName() + "--" + fileName));
+		}
+		this.driver.manage().deleteAllCookies();
+		this.driver.navigate().refresh();
+	}
+
+	@AfterClass
+	public void tearDown() {
+		this.driver.quit();
+	}
 }
