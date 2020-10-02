cd279dfd1d421ff25dc1c38b2682d35da10d6a0c
Ivica Nikolic <ivicanklc@gmail.com>
2020-10-02 16:10:01 +0200

    PageTest class is complete

--- src/test/ProfileTest.java
+++ src/test/ProfileTest.java
@@ -1,5 +1,8 @@
 package test;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.testng.annotations.Test;
 
 import pages.AuthPage;
@@ -10,8 +13,14 @@ import pages.ProfilePage;
 
 public class ProfileTest extends BaseTest {
 
-	@Test(priority = 5, description="login, editing profile, logout.")
+	@Test(priority = 5, description = "Testing LogIn, Editing Profile and Logout.")
 	public void editProfile() throws InterruptedException {
+
+		LocationPopupPage locationPopupPage = new LocationPopupPage(driver, wait, jsExecutor);
+		LoginPage loginPage = new LoginPage(driver, wait, jsExecutor);
+		NotificationSistemPage notificationSistemPage = new NotificationSistemPage(driver, wait, jsExecutor);
+		ProfilePage profilePage = new ProfilePage(driver, wait, jsExecutor);
+		AuthPage authPage = new AuthPage(driver, wait, jsExecutor);
 
 		String name = "customer@dummyid.com";
 		String pass = "12345678a";
@@ -23,6 +32,32 @@ public class ProfileTest extends BaseTest {
 		String country = "United Kingdom";
 		int stateIndex = 2;
 		int cityIndex = 1;
+		String message = "";
+
+		// LogIn
+		this.driver.navigate().to(baseUrl + "guest-user/login-form");
+		locationPopupPage.closePopup();
+		loginPage.login(name, pass);
+
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Login Successfull"), "[ERROR] Login FAILED!");
+
+		// Editing Profile
+		this.driver.navigate().to(baseUrl + "member/profile");
+		profilePage.changeBasicInfo(firstName, lastName, address, phone, zip, country, stateIndex, cityIndex);
+
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Setup Successful"), "[ERROR] Unexpected profile update message");
+
+		// LogOut
+		authPage.logout();
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Logout Successful!"), "[ERROR] Logout FAILED!");
+	}
+
+	
+	@Test(priority = 10, description = "Testing the Functionality of Adding and Deleting Profile Images")
+	public void changeProfileImage() throws IOException, InterruptedException {
 
 		LocationPopupPage locationPopupPage = new LocationPopupPage(driver, wait, jsExecutor);
 		LoginPage loginPage = new LoginPage(driver, wait, jsExecutor);
@@ -30,25 +65,45 @@ public class ProfileTest extends BaseTest {
 		ProfilePage profilePage = new ProfilePage(driver, wait, jsExecutor);
 		AuthPage authPage = new AuthPage(driver, wait, jsExecutor);
 
+		String name = "customer@dummyid.com";
+		String pass = "12345678a";
+		String message = "";
+		// String imgPath = new File("imagеs/nestor.jpg").getCanonicalPath();
+		String imgPath = "C:\\Users\\ivica\\Desktop\\Zavrsni Projekat\\-Yo-Meals-project\\images\\nestor.jpg";
+
+		// LogIn
 		this.driver.navigate().to(baseUrl + "guest-user/login-form");
-		
 		locationPopupPage.closePopup();
 		loginPage.login(name, pass);
 
-		String messageOne = notificationSistemPage.getMessage();
-		this.softAssert.assertTrue(messageOne.contains("Login Successfull"), "[ERROR] Unexpected login message");
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Login Successfull"), "[ERROR] Login FAILED!");
 
+		// Upload Image
 		this.driver.navigate().to(baseUrl + "member/profile");
-		profilePage.changeBasicInfo(firstName, lastName, address, phone, zip, country, stateIndex, cityIndex);
+		profilePage.uploadImage(imgPath);
 
-		String messageTwo = notificationSistemPage.getMessage();
-		System.out.println(messageTwo);
-		this.softAssert.assertTrue(messageOne.contains("Setup Successfull"),
-				"[ERROR] Unexpected profile update message");
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Profile Image Uploaded Successfully"),
+				"[ERROR] Image is not uploaded!!");
 
+		notificationSistemPage.noticeIsNotVisible();
+
+		// Delete Image
+		profilePage.deleteImage();
+
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Profile Image Deleted Successfully"),
+				"[ERROR] The image is not deleted");
+
+		notificationSistemPage.noticeIsNotVisible();
+
+		// LogOut
 		authPage.logout();
-		String messageThree = notificationSistemPage.getMessage();
-		this.softAssert.assertTrue(messageThree.contains("Logout Successfull!"), "[ERROR] Unexpected logout message");
 
+		message = notificationSistemPage.getMessage();
+		this.softAssert.assertTrue(message.contains("Logout Successfull!"), "[ERROR] Logout FAILED!");
+
+		softAssert.assertAll();
 	}
 }
